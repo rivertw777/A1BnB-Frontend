596947fb8eb789105f0728dd176eb73e45a7276e
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-02-13 22:27:32 +0900

    feat: Logout 수정

--- src/pages/users/Logout.js
+++ src/pages/users/Logout.js
@@ -1,10 +1,23 @@
 import React, { useEffect } from "react";
 import { useAppContext, deleteToken } from "../../store";
 import { useNavigate } from "react-router-dom";
+import { axiosInstance, useAxios } from "../../api";
 
 export default function Logout() {
   const { dispatch } = useAppContext();
   const navigate = useNavigate();
+
+  const {
+    store: { jwtToken }
+  } = useAppContext();
+
+
+  const headers = { Authorization: `Bearer ${jwtToken}` };
+
+  const [] = useAxios({
+    url: "/api/security/logout",
+    headers
+  });
 
   useEffect(() => {
     dispatch(deleteToken());
